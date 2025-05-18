5a5de79f4542c2fc53f13067d6db7d5f245848ca
23CSBRAGULR <ragulraguraman1269@gmail.com>
2025-05-18 15:07:10 +0530

    Added Comments to AlphabetOrNot

--- AlphabetOrNot.java
+++ AlphabetOrNot.java
@@ -1,3 +1,5 @@
+//a Program that fetches a character from the user and checks whether it is Alphabetic
+
 import java.util.Scanner;
 
 public class AlphabetOrNot {
@@ -6,13 +8,16 @@ public class AlphabetOrNot {
         System.out.print("Enter the Character to check wheather it is a Alphabet or Nah : ");
         String input = inputs.next();
 
+        //a loop to check whether the user entered only a single character
         while(input.length() > 1) {
             System.out.println("One Character at a time YOU IDIOT!");
             input = inputs.next();
         }
 
+        //Scanner takes the input as String hence the type change
         char check = input.charAt(0);
 
+        //a if-else statement that uses ASCII values to check if the character is an Alphabet
         if(check >= 'a' && check <= 'z' || check >= 'A' && check <= 'Z') {
             System.out.println("Yes it is.");
         }
@@ -20,7 +25,9 @@ public class AlphabetOrNot {
             System.out.println("No its not.");
         }
 
+        //an integer variable that stores the ASCII value of the character
         int ascii = (int)check;
-        System.out.println(ascii);
+        System.out.println("BTW the ASCII value of the given character is " + ascii);
+        inputs.close();
     }
 }
