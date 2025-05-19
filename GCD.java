5ebe280f492c1f90526c07a85696d60e1e3e6532
23CSBRAGULR <ragulraguraman1269@gmail.com>
2025-05-19 14:46:05 +0530

    Added comments to the GCD file

--- GCD.java
+++ GCD.java
@@ -1,7 +1,11 @@
+//the method I came up with for finding the GCD of any two numbers
+//less efficient than Euclidean's method
+
 import java.util.Scanner;
 
 public class GCD {
 
+    //the method that returns the GCD of two numbers by checking the divisibility of the exact divisors of the smallest number with the largest number
     static int gcdCalculation(int divisors[], int largest) {
         int gcd = 1;
         for(int i = (divisors.length - 1); i >= 0; i--) {
@@ -23,12 +27,16 @@ public class GCD {
 
         int smallest = Math.min(firstNumber, secondNumber);
         int largest = Math.max(firstNumber, secondNumber);
+        
+        //a for loop for determining the length of an array which will contain all the exact divisors of the smallest number
         int length = 0;
         for(int i = 1; i <= smallest; i++) {
             if(smallest%i == 0) {
                 length++;
             }
         }
+
+        //divisors array creation and insertion
         int divisors[] = new int[length];
         int index = 0;
         for(int i = 1; i <= smallest; i++) {
@@ -39,5 +47,6 @@ public class GCD {
         }
         int gcd = gcdCalculation(divisors, largest);
         System.out.println("The Greatest Common Divisor(GCD) of " + firstNumber + " and " + secondNumber + " is " + gcd + ".");
+        inputs.close();
     }
 }
