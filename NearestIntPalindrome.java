876a69ebfb07b7c209dea55b5c060398d44f4651
Viji Lakshmi <ragulraguraman1269@gmail.com>
2025-06-28 19:00:48 +0530

    New Program in process

---
+++ NearestIntPalindrome.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class NearestIntPalindrome {
+
+    
+    public static void main(String[] args) {
+        Scanner inputs = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = inputs.nextInt();
+        inputs.close();
+    }
+}
